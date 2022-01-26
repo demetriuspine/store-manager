3abcc3a0aaa8eeff1632e0666f8da6a8db6be099
lucas <mr.pine97@gmail.com>
2022-01-26 18:57:29 -0300

    req2 - feat: cria a controller

--- controllers/products.js
+++ controllers/products.js
@@ -19,7 +19,21 @@ const create = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await productServices.getById(id);
+    if (!product || product.length === 0) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   create,
   getAll,
+  getById,
 };
