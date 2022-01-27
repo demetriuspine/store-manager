fd93bce05a9654a63fa1e47ef600de09c2fe2e59
lucas <mr.pine97@gmail.com>
2022-01-27 16:19:13 -0300

    req3 - feat: cria a rota

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const productsController = require('../controllers/products');
 const productExistence = require('../middlewares/products/productExistence');
+const validateExistence = require('../middlewares/products/validateExistence');
 const validateName = require('../middlewares/products/validateName');
 const validateQuantity = require('../middlewares/products/validateQuantity');
 
 const router = express.Router();
 
 router.get('/', productsController.getAll);
-router.get('/:id', productsController.getById);
+router.get('/:id', validateExistence, productsController.getById);
+router.put('/:id', validateQuantity, validateName, validateExistence, productsController.update);
 router.post('/', validateQuantity, validateName, productExistence, productsController.create);
 
 module.exports = router;
