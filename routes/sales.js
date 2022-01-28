36460a38bfbc0296822746af98e59f7cb53bea15
lucas <mr.pine97@gmail.com>
2022-01-28 16:30:45 -0300

    req6 - feat: cria a rota

--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const salesController = require('../controllers/sales');
+const validateExistence = require('../middlewares/sales/validateExistence');
 const validateId = require('../middlewares/sales/validateId');
 const validateQuantity = require('../middlewares/sales/validateQuantity');
 const validateQuantityValue = require('../middlewares/sales/validateQuantityValue');
@@ -7,6 +8,7 @@ const validateQuantityValue = require('../middlewares/sales/validateQuantityValu
 const router = express.Router();
 
 router.get('/', salesController.getAll);
+router.get('/:id', validateExistence, salesController.getById);
 router.post('/', validateId, validateQuantity, validateQuantityValue, salesController.create);
 
 module.exports = router;
