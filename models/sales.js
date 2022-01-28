42df7bda407290e5ea82a4ef58c892b73cec282b
lucas <mr.pine97@gmail.com>
2022-01-28 15:39:16 -0300

    req5 - feat: cria as queries e executa

---
+++ models/sales.js
@@ -0,0 +1,35 @@
+const connection = require('./connection');
+
+const createNewSale = async () => {
+  const [rows] = await connection.execute('INSERT INTO StoreManager.sales VALUE ()');
+  return rows.insertId;
+};
+
+const create = async (sales) => {
+  const id = await createNewSale();
+  const query = 'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUE ?';
+  const newSaleValues = sales.map((sale) => [id, sale.product_id, sale.quantity]);
+  await connection.query(
+    query, [newSaleValues],
+  );
+  return {
+    id,
+    itemsSold: sales,
+  };
+};
+
+const getAll = async () => {
+  const query = `SELECT p.sale_id AS saleId, s.date, p.product_id, p.quantity
+                FROM
+                    StoreManager.sales_products AS p
+                INNER JOIN
+                    StoreManager.sales AS s
+                ON p.sale_id = s.id;`;
+  const [rows] = await connection.execute(query);
+  return rows;
+};
+
+module.exports = {
+  create,
+  getAll,
+};
